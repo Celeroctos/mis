ed969e392346980acf0102fd940ff2f8269ef88b
Dzhamal Tayibov <prohps@yandex.ru>
2015-02-25 22:10:43 +0300

    Gramotnaya BEM verstka, fix fix ...

--- protected/widgets/views/footerWidget.php
+++ protected/widgets/views/footerWidget.php
@@ -1,17 +1,13 @@
 <?php
 /**
- * Шапка
+ * Подвал
  * @author Dzhamal Tayibov <prohps@yandex.ru>
  */
 ?>
-<div class="b-footer">
-	<div class="container">
-		<div class="b-footer__body">
-			<div class="row">
-				<div class="col-xs-12">
-					FOOTER
-				</div>
-			</div>
+<div class="container b-footer">
+	<div class="row">
+		<div class="col-xs-12">
+			FOOTER
 		</div>
 	</div>
 </div>
--- protected/widgets/views/headerWidget.php
+++ protected/widgets/views/headerWidget.php
@@ -4,17 +4,13 @@
  * @author Dzhamal Tayibov <prohps@yandex.ru>
  */
 ?>
-<div class="b-header">
-	<div class="container">
-		<div class="b-header__body">
-			<div class="row">
-				<div class="col-xs-2">
-					МИС Notum
-					<?php if(!Yii::app()->user->isGuest): ?>
-						<?= CHtml::link('Выйти', Yii::app()->createUrl('service/auth/logout'), ['class'=>'btn btn-default btn-block btn-sm']); ?>
-					<?php endif; ?>
-				</div>
-			</div>
+<div class="container b-header">
+	<div class="row">
+		<div class="col-xs-12">
+			МИС Notum
+			<?php if(!Yii::app()->user->isGuest): ?>
+				<?= CHtml::link('Выйти', Yii::app()->createUrl('service/auth/logout'), ['class'=>'btn btn-default btn-sm']); ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
