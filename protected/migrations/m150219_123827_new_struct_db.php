d8b7d734d7b3bd17c205d49c3f6ec90cf8ea8887
Dzhamal <prohps@yandex.ru>
2015-03-05 10:55:36 +0300

    migration for new core

--- protected/migrations/m150219_123827_new_struct_db.php
+++ protected/migrations/m150219_123827_new_struct_db.php
@@ -17,34 +17,61 @@ HERE;
 		$command->execute();
 		
 		$sql=<<<HERE
-				CREATE TABLE IF NOT EXISTS "mis"."patients"
+				CREATE TABLE IF NOT EXISTS "mis"."patients" --Пациенты
 				(
 					"patient_id" serial NOT NULL,
 					"first_name" character varying(255) NOT NULL,
 					"middle_name" character varying(255) NOT NULL,
 					"last_name" character varying(255) NOT NULL,
 					"gender" character varying(100) NOT NULL, --1/2 (Мужской/женский)
-					"birthday" date DEFAULT NULL,
-					"document_type" character varying(255) DEFAULT NULL, --Тип документа (паспорт и прочее)
-					"document_serie" character varying(255) DEFAULT NULL, --Серия документа
-					"document_number" character varying(255) DEFAULT NULL, --Номер документа
-					"document_who_gived" character varying(255) DEFAULT NULL, --Кто выдал документ
-					"document_date_gived" character varying(255) DEFAULT NULL, --Дата выдачи
+					"birthday" date NOT NULL, --День рождения
 					"address_reg" character varying(255) DEFAULT NULL, --Адрес регистрации
 					"address" character varying(255) DEFAULT NULL, --Адрес фактического проживания
 					"snils" character varying(255) DEFAULT NULL, --СНИЛС
 					"invalid_group" character varying(255) DEFAULT NULL, --группа инвалидности
 					"phone_number" character varying(255) DEFAULT NULL, --Номер телефона
 					"profession" character varying(255) DEFAULT NULL, --Профессия
-					"job_address" character varying(255) DEFAULT NULL, --Адрес работы
+					"work_address" character varying(255) DEFAULT NULL, --Адрес работы
+					"work_place" character varying(255) DEFAULT NULL, --Место работы
+					"address_str" text DEFAULT NULL, --for search
+					"address_reg_str" text DEFAULT NULL, --for search
 					"create_timestamp" TIMESTAMPTZ NOT NULL, --Время создания записи (пациента)
 					PRIMARY KEY(patient_id)
 				);
 HERE;
 		$command=$connection->createCommand($sql);
 		$command->execute();
+		
+		$sql=<<<HERE
+				CREATE TABLE IF NOT EXISTS "mis"."patient_documents" --Документы гражданина
+				(
+					"patient_document_id" serial NOT NULL,
+					"serie" character varying(255) DEFAULT NULL,
+					"number" character varying(255) DEFAULT NULL,
+					"who_gived" character varying(255) DEFAULT NULL,
+					"date_gived" character varying(255) DEFAULT NULL,
+					"type" integer DEFAULT NULL,
+					"patient_id" integer DEFAULT NULL, --FK (table mis.patients)
+					PRIMARY KEY(patient_document_id)
+				);
+HERE;
+		$command=$connection->createCommand($sql);
+		$command->execute();
+		
+		$sql=<<<HERE
+				CREATE TABLE IF NOT EXISTS "mis"."patient_contacts"
+				(
+					"patient_contact_id" serial NOT NULL,
+					"patient_id" integer DEFAULT NULL, --FK (table mis.patients)
+					"type" integer DEFAULT NULL, --Тип контакта (1/2) 1-Мобильный, 2-домашний
+					"value" character varying(255) DEFAULT NULL,
+					PRIMARY KEY(patient_contact_id)
+				);
+HERE;
+	
+		$command=$connection->createCommand($sql);
+		$command->execute();
 	}
-
 	public function down()
 	{
 	}
